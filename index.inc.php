4bd4d17c933904503797d683a9acb731ead61658
dzdev <79674132@qq.com>
2013-04-07 22:33:05 +0800

    add action flow

--- index.inc.php
+++ index.inc.php
@@ -4,4 +4,8 @@ if(!defined('IN_DISCUZ')) {
 }
 
 $setting = $_G['cache']['plugin']['carnum'];   // inital vars
+$action = trim($_G['gp_action']);
+if(empty($action)) {
+    echo 'please choice the action';
+}
 ?>
